dc00985dde17d50a7daebf5b679b79312e7afdec
vikalpnimesh <vikalpnimesh786@gmail.com>
2023-08-30 21:08:32 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [bg, setbg] = useState("white")
+
+
+
+  return (
+
+    <body style={{backgroundColor:bg}} className="  m-0 p-0"> 
+
+       
+      <div >
+      <h1 className="mb-5">Back-Ground Color Changer</h1>
+        <button className="m-1 text-black" style={{backgroundColor:'red'}} onClick={()=>setbg("red")}>RED</button>
+        <button className="m-1 text-black" style={{backgroundColor:'orange'}} onClick={()=>setbg("orange")}>orange</button>
+        <button className="m-1 text-black" style={{backgroundColor:'blue'}} onClick={()=>setbg("blue")}>blue</button>
+        <button className="m-1 text-black" style={{backgroundColor:'yellow'}} onClick={()=>setbg("yellow")}>yellow</button>
+        <button className="m-1 text-black" style={{backgroundColor:'green'}} onClick={()=>setbg("green")}>green</button>
+        <button className="m-1 text-royalblue" style={{backgroundColor:'royalblue'}} onClick={()=>setbg("royalblue")}>ROYALBLUE</button>
+        <button className="m-1 text-black" style={{backgroundColor:'grey'}} onClick={()=>setbg("grey")}>grey</button>
+      </div> 
+    </body>
+
+  );
+}
+
+export default App;
+
+       
---
+++ src/component/counter.jsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  let [Numb, setNumb] = useState(0);
+
+  function add() {
+    if (Numb == 20) {
+      return;
+    } else {
+      Numb = Numb + 1;
+      setNumb(Numb);
+    }
+  }
+  function remove() {
+    if (Numb == 0) {
+      return;
+    }
+
+    Numb = Numb - 1;
+    setNumb(Numb);
+  }
+  function reset(){
+   setNumb( Numb=0)
+
+  }
+
+  return (
+    <>
+      <h1 className="p-5 w-96  shadow rounded font-medium">Counter Button</h1>
+      <h2>Counter : {Numb}</h2>
+      <hr className="m-5" />
+      <button onClick={remove} className="bg-blue ml-7 p-6 font-bold font-serif">Remove</button>
+      {/* <hr /> */}
+      <button onClick={reset} className="bg-blue ml-7 p-6 font-bold  font-serif">Reset</button>
+      {/* <hr /> */}
+      <button onClick={add} className="bg-blue ml-7 p-6 font-bold  font-serif">Add</button>
+     
+      <hr  className="m-5"/>
+      
+    </>
+  );
+}
+
+export default App;
